f83bc3e183d59c5beeb375ad2b8968b7924774eb
Armando Rios <armandorios.dev@gmail.com>
2025-10-19 09:10:46 -0700

    chore: configure linting and formatting

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: ["node_modules", "dist", "coverage", "*.config.*"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier,
+    },
+    rules: {
+      ...tseslint.configs["recommended"].rules,
+      ...prettierConfig.rules,
+      "prettier/prettier": "error",
+    },
+  },
+];
