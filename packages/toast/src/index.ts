d998171247c633cc76d513d27e47ea38f96cbfaf
Armando Rios <armandorios.dev@gmail.com>
2025-10-23 14:06:11 -0700

    feat(toast): implement convenience methods

--- packages/toast/src/index.ts
+++ packages/toast/src/index.ts
@@ -1,3 +1,21 @@
 import { ToastManager } from './ToastManager'
+import type { ToastOptions } from './types'
 
 export const toastManager = new ToastManager()
+
+export const toast = {
+  success: (message: string, options?: ToastOptions) =>
+    toastManager.create(message, { ...options, type: 'success' }),
+
+  error: (message: string, options?: ToastOptions) =>
+    toastManager.create(message, { ...options, type: 'error' }),
+
+  warning: (message: string, options?: ToastOptions) =>
+    toastManager.create(message, { ...options, type: 'warning' }),
+
+  info: (message: string, options?: ToastOptions) =>
+    toastManager.create(message, { ...options, type: 'info' }),
+
+  default: (message: string, options?: ToastOptions) =>
+    toastManager.create(message, { ...options, type: 'default' }),
+}
